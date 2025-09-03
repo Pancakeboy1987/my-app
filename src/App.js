ba62f75c547d077da30d7ff569c7f1248bf15ec4
Akechi Goro <zane41181@gmail.com>
2025-09-03 13:35:14 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+
+      <div className="upper-block">
+        janwcjn
+      </div>
+
+      <div className="main-block">
+        <div className="left-block">
+          <p>
+          This is the start
+          </p>
+        </div>
+        <div className="right-block">
+
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
-        </a>
-      </header>
+          
+          </a>
+        </div>
+      </div>
+
+
+
+
     </div>
   );
 }
