705e239093acf8a70487a23388fcda6012c6916b
Akechi Goro <zane41181@gmail.com>
2025-09-11 18:30:03 +0300

    .

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,6 +1,13 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import ChatRender from "./ChatRender";
 
 export default function MenuBar(){
+    const [page, setPage] = useState("chats");
 
+    const pages = {
+      chats: <ChatRender/>,
+      profile: <Profile />,
+      settings: <Settings />
+    };
 }
